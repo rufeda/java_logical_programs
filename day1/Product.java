c50a66eee1b65a2ea10ca105f1ceda354fb61aa9
rufeda <rufedaabdul@gmail.com>
2024-11-07 10:58:57 +0530

    Add files via upload

---
+++ day1/Product.java
@@ -0,0 +1,15 @@
+class Product
+{
+public static void main(String[] args)
+{
+int pid=11432;
+String pname="laptop";
+String brand="acer";
+float cost=8888.88f;
+
+System.out.println("product id is:"+pid);
+System.out.println("product name is:"+pname);
+System.out.println("product brand is:"+brand);
+System.out.println("product cost is:"+cost);
+}
+}
---
+++ day1/Student.java
@@ -0,0 +1,17 @@
+class Student
+{
+public static void main(String args[])
+{
+int stid=06;
+String sname="rufeda";
+long phno=983215767;
+char gen='f';
+String course="MCA";
+
+System.out.println("student id is:"+stid);
+System.out.println("student name is:"+sname);
+System.out.println("student phone number is:"+phno);
+System.out.println("student gender is:"+gen);
+System.out.println("student course is:"+course);
+}
+}
